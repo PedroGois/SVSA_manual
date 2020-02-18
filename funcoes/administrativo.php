24ef515c7260486798d2df16f6cd6541a3d17633
PedroGois <41579818+PedroGois@users.noreply.github.com>
2020-02-18 17:27:41 -0300

    Add files via upload
    PRIMEIRO COMMIT

---
+++ funcoes/administrativo.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="charset" content="utf-8"/>
+        <title>ADMINISTRATIVO</title>
+        <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="../css/style.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
+        <a class="navbar-brand text-nav" href="#">MANUAL SVSA</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link text-light" href="../welcome.php">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-light" href="#">Link</a>
+            </li>
+            
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+</nav>
+
+<div class="secao  bg-info text-light ">
+        <h1 class="display-4">ADMINISTRATIVO</h1>
+        <hr class="my-4">  
+        <p class="lead"> Funcionalidades que podem ser acessadas pelo administrador
+</p>
+        <hr class="my-4">  
+    </div>
+    </body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="charset" content="utf-8"/>
+        <title>Manual SVSA</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="css/style.css" rel="stylesheet" type="text/css" />
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    </head>
+    <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
+  <a class="navbar-brand text-nav" href="#">MANUAL SVSA</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link text-light" href="../welcome.php">HOME <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-light" href="#">FUNCIONALIDADES BASICAS</a>
+      </li>
+      
+    </ul>
+    
+  </div>
+</nav>
+
+    <div class="secao  bg-info text-light ">
+        <h1 class="display-4">SOBRE O MANUAL</h1>
+        <hr class="my-4">  
+        <p class="lead">    Este manual tem a finalidade de servir de guia para os usuários do SVSA, facilitando o acesso ao sistema e suas funcionalidades.
+</p>
+        <hr class="my-4">  
+    </div>
+
+    <div class="secao funcoes bg-light ">
+        <h1 class="display-4 text-center text-info">FUNCIONALIDADES</h1>
+        <hr class="my-4">  
+        <div class="container">
+                <div class="row text-center" >
+                    <div class="col-4 listas">
+                        <a class="text-info" href="funcoes/administrativo.php">PERFIL ADMINISTRATIVO</a>  
+                        <hr class="my-4">  
+                        <ul class="text-dark">
+                            <li>
+                            dashboard
+                            </li>
+                            <li>
+                            dashboard
+                            </li>
+                            <li>
+                            dashboard
+                            </li>
+                            <li>
+                            dashboard
+                            </li>
+                            <li>
+                            dashboard
+                            </li>
+                        </ul>    
+                    </div>
+                    <div class="col-4 listas">
+                    <a class="text-info" href="funcoes/administrativo.php">PERFIL CADASTRADOR</a>
+                        <hr class="my-4">  
+                        <ul class="text-dark">
+                            <li>
+                            dashboard
+                            </li>
+                            <li>
+                            dashboard
+                            </li>
+                            <li>
+                            dashboard
+                            </li>
+                            <li>
+                            dashboard
+                            </li>
+                            <li>
+                            dashboard
+                            </li>
+                        </ul> 
+                    </div>
+                    <div class="col-4 listas">
+                    <a class="text-info" href="funcoes/administrativo.php">PERFIL TECNICO</a>
+                        <hr class="my-4">
+                        <ul class="text-dark">
+                            <li>
+                            dashboard
+                            </li>
+                            <li>
+                            dashboard
+                            </li>
+                            <li>
+                            dashboard
+                            </li>
+                          
+
+                        </ul>   
+                    </div>
+        </div>
+        <div class="container">
+                <div class="row text-center">
+                    <div class="col-6 listas">
+                    <a class="text-info" href="funcoes/administrativo.php">PERFIL COORDENADOR</a> 
+                        <hr class="my-4">   
+                        <ul class="text-dark">
+                            <li>
+                            dashboard
+                            </li>
+                            <li>
+                            dashboard
+                            </li>
+                            <li>
+                            dashboard
+                            </li>
+                            <li>
+                            dashboard
+                            </li>
+                            <li>
+                            dashboard
+                            </li>
+                        </ul>           
+                    </div>
+                    <div class="col-6 listas">
+                        <a class="text-info" href="funcoes/administrativo.php">PERFIL GESTOR</a>
+                        <hr class="my-4">  
+                        <ul class="text-dark">
+                            <li>
+                            dashboard
+                            </li>
+                            <li>
+                            dashboard
+                            </li>
+                            <li>
+                            dashboard
+                            </li>
+                            <li>
+                            dashboard
+                            </li>
+                            <li>
+                            dashboard
+                            </li>
+                        </ul> 
+                    </div>
+        </div>
+    </div> 
+  
+    </body>
+</html>
